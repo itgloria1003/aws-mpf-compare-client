976bfe70dfadfe0e4a15a601d34a254dd1d23543
gloriatse <gloriatse@192.168.0.104>
2017-04-24 00:19:33 +0800

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,12 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_fund_type_list :'http://localhost:8080/rest/list',
+  api_fund_type_action:'http://localhost:8080/rest/action?requestAction=',
+  api_fund_type_stat: 'http://localhost:8080/rest/stat?asOfDate=',
+  api_fund_detail: 'http://localhost:8080/rest/details',
+  api_fund_type_filter: 'http://localhost:8080/rest/mpffilter',
+  api_rss_to_json: 'https://rss2json.com/api.json?rss_url=',
+  rss_mpfa_press:'http://www.mpfa.org.hk/eng/main/rss/xml/press_rss.xml'
 };
