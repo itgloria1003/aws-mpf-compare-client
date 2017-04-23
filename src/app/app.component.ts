f65af04216d786a572a21a3a6ca0e186aeb26e78
gloriatse <gloriatse@192.168.0.104>
2017-04-24 00:05:14 +0800

    modify for assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { ListFundTypeComponent } from './list-fund-type/list-fund-type.component';
+import { MpfFundDetailComponent } from './mpf-fund-detail/mpf-fund-detail.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { RiskAssessmentComponent} from './risk-assessment/risk-assessment.component';
+import { MpfFundService } from './mpf-fund.service';
+import { FeedService } from './feed-service.service';
+import { OperationResult } from './operation-result';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MpfFundService,FeedService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'MPF compare';
+  activeTab : number  ;
+  isLoading : boolean = false;
+  actionResult: OperationResult= new OperationResult();
+  touched: boolean = false;
+  operationResult: Observable<OperationResult>;
+  listFundTypeComp: ListFundTypeComponent;
+  mpfFundDetailComp: MpfFundDetailComponent;
+
+  constructor(private listMpfService: MpfFundService) {
+     this.activeTab = 0 ;
+  }
+
+  selectTab(tabIndex:number){
+    this.activeTab = tabIndex; 
+   
+  }
+
+ 
+  onClickScrape() {
+    console.log('Start scraping data');
+    this.touched = true;
+    this.isLoading = true;
+    this.operationResult = this.listMpfService.performAction('scrape');
+    this.operationResult.subscribe(details => {
+      console.log('Complete scraping data');
+       this.isLoading = false; 
+      this.actionResult = details;
+      
+    });
+   
+  }
+
+  onClickDelete() {
+    console.log('Start deleting the cache');
+    this.touched = true;
+    this.isLoading= true;
+    this.listMpfService.performAction('delete').subscribe(details => {
+      console.log('Complete deleting the cache');
+      this.isLoading = false; 
+       this.actionResult = details;
+
+    });
+    
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+// NG2-Charts
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+//Reactive forms
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
+import { ListFundTypeComponent } from './list-fund-type/list-fund-type.component';
+import { MpfFundService} from './mpf-fund.service';
+import { Nl2brPipePipe } from './nl2br-pipe.pipe';
+import { FundTypeChartComponent } from './fund-type-chart/fund-type-chart.component';
+import { MpfFundDetailComponent } from './mpf-fund-detail/mpf-fund-detail.component';
+import { NullToNaPipePipe } from './null-to-na-pipe.pipe';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { FeedCardComponent } from './feed-card/feed-card.component';
+import { StripHtmlTagsPipe } from './pipe/strip-html-tags.pipe';
+import { RiskAssessmentComponent } from './risk-assessment/risk-assessment.component'; 
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListFundTypeComponent,
+    Nl2brPipePipe,
+    FundTypeChartComponent,
+    MpfFundDetailComponent,
+    NullToNaPipePipe,
+    SpinnerComponent,
+    FeedCardComponent,
+    StripHtmlTagsPipe,
+    RiskAssessmentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [MpfFundService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
